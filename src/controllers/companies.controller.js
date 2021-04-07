edbd9c354b1345c4e5ada3ad91018f7a5be541ff
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-07 15:43:56 -0500

    :sparkles: company signUp

--- src/controllers/companies.controller.js
+++ src/controllers/companies.controller.js
@@ -1,7 +1,27 @@
 const companyCtrl = {};
+const mongoose = require('mongoose');
 
-companyCtrl.signUp = (req, res) => {
-  res.json({ message: 'hola registro' });
+const { Company } = require('../models/entity.model');
+
+companyCtrl.signUp = async (req, res) => {
+  const reqCompany = req.body;
+
+  const document = new Company(reqCompany);
+  document.user.role = 'company';
+  document.user.password = await document.user.encryptPassword(document.user.password);
+
+  await document
+    .save()
+    .then(() => {
+      res.status(200).json({ message: 'registro completo' });
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError)
+        return res
+          .status(400)
+          .json({ message: 'Incomplete or bad formatted client data', errors: err.errors });
+      return res.status(500).json({ message: `internal server error  ${err}` });
+    });
 };
 
 module.exports = companyCtrl;
