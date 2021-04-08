4bff0339f3ccfd204d1fb1022be20d68f65eead9
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-07 20:14:11 -0500

    :sparkles: enpoints profile and update profile

--- src/routes/companies.routes.js
+++ src/routes/companies.routes.js
@@ -4,5 +4,7 @@ const router = Router();
 const companiesController = require('../controllers/companies.controller');
 
 router.post('/', companiesController.signUp);
+router.get('/', companiesController.profile);
+router.put('/', companiesController.updateProfile);
 
 module.exports = router;
