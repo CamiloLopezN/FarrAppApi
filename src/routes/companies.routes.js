a6618c547734225114b9dd79035be5a500490d80
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-08 15:13:30 -0500

    :sparkles: endpoint for register establishment

--- src/routes/companies.routes.js
+++ src/routes/companies.routes.js
@@ -6,5 +6,6 @@ const companiesController = require('../controllers/companies.controller');
 router.post('/', companiesController.signUp);
 router.get('/', companiesController.profile);
 router.put('/', companiesController.updateProfile);
+router.post('/establishments', companiesController.postEstablishment);
 
 module.exports = router;
