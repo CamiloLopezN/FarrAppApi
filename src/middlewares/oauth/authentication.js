af19a714f7580045d0480b217268a01b1f35e57d
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-18 23:42:33 -0500

    :sparkles: method for authorization admin or company

--- src/middlewares/oauth/authentication.js
+++ src/middlewares/oauth/authentication.js
@@ -15,7 +15,7 @@ oauth.authentication = async (req, res, next) => {
       .status(401)
       .json({ message: 'Acceso no autorizado, se debe proporcionar un token valido' }); // if there isn't any token
 
-  await jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+  await jwt.verify(token, process.env.KEY_SECRET_TOKEN, (err, payload) => {
     if (err) return res.status(403).json({ message: 'Usuario invalido, el token no es valido' });
     req.payload = payload;
     return next();
@@ -37,8 +37,22 @@ oauth.authorizationCompany = async (req, res, next) => {
   return next();
 };
 
+oauth.authorizationAdminOrCompany = (req, res, param) => {
+  let id;
+  if (req.payload.role === roles.adminRole) {
+    id = param;
+  } else if (req.payload.role === roles.companyRole) {
+    if (!req.payload.roleId === param)
+      return res.status(400).json({ message: 'Incomplete or bad formatted client data' });
+    id = param;
+  } else {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+  return id;
+};
+
 oauth.generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_KEY, {
+  return jwt.sign(payload, process.env.KEY_SECRET_TOKEN, {
     expiresIn: config.expiresSessionIn,
   });
 };
