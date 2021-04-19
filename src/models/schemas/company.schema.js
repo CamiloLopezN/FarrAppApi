0e111d1860235aa110fd7f9ec8bf8d4a0756dd7f
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-18 21:15:06 -0500

    :sparkles: activate timestamps of mongoose

--- src/models/schemas/company.schema.js
+++ src/models/schemas/company.schema.js
@@ -4,15 +4,20 @@ const { Schema } = require('../../config/config.database');
 const eventPreviewSchema = require('./eventPreview.schema');
 const establishmentPreviewSchema = require('./establishmentPreview.schema');
 
-const companySchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, required: true, unique: true, ref: 'Users' },
-  companyName: { type: String, unique: true, required: true },
-  address: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  nit: { type: String, required: true, unique: true },
-  establishments: [establishmentPreviewSchema],
-  events: [eventPreviewSchema],
-});
+const companySchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, required: true, unique: true, ref: 'Users' },
+    companyName: { type: String, unique: true, required: true },
+    address: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    nit: { type: String, required: true, unique: true },
+    establishments: [establishmentPreviewSchema],
+    events: [eventPreviewSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
 
 companySchema.plugin(uniqueValidator);
 
