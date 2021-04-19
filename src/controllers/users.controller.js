b19db505855432a1e5542c8976906f6a7292ae95
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-18 21:13:34 -0500

    :sparkles: universal login for users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,27 +1,42 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const { Company } = require('../models/entity.model');
+const { User, Company, Admin, Client } = require('../models/entity.model');
+const roles = require('../middlewares/oauth/roles');
 const { generateToken } = require('../middlewares/oauth/authentication');
 
 module.exports.login = async (req, res) => {
   const { email, password } = req.body;
   let token;
+  let roleId;
   try {
-    const doc = await Company.findOne({ 'user.email': email });
-    if (!doc) return res.status(400).send({ message: 'Incomplete or bad formatted data' });
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send({ message: 'Incomplete or bad formatted data' });
 
-    if (doc.user.isVerify && !doc.user.isVerify) {
+    if (!user.isVerified) {
       return res.status(403).send({ message: 'Forbidden' });
     }
 
-    if (!(await bcrypt.compare(password, doc.user.password))) {
+    if (!(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ message: 'Incomplete or bad formatted client data' });
+    }
+
+    if (user.role === roles.companyRole) {
+      // eslint-disable-next-line no-underscore-dangle
+      roleId = await Company.findOne({ userId: user._id }, { _id: 1 });
+    } else if (user.role === roles.adminRole) {
+      // eslint-disable-next-line no-underscore-dangle
+      roleId = await Admin.findOne({ userId: user._id }, { _id: 1 });
+    } else {
+      // eslint-disable-next-line no-underscore-dangle
+      roleId = await Client.findOne({ userId: user._id }, { _id: 1 });
     }
 
     const payload = {
       // eslint-disable-next-line no-underscore-dangle
-      id: doc._id,
-      role: doc.user.role,
+      userId: user._id,
+      // eslint-disable-next-line no-underscore-dangle
+      roleId: roleId._id,
+      role: user.role,
     };
 
     token = await generateToken(payload);
@@ -30,7 +45,7 @@ module.exports.login = async (req, res) => {
       return res
         .status(400)
         .json({ message: 'Incomplete or bad formatted client data', errors: err.errors });
-    res.status(500).json({ message: `internal server error  ${err}` });
+    return res.status(500).json({ message: `internal server error  ${err}` });
   }
-  return res.status(200).json({ token });
+  return res.status(200).json({ message: token });
 };
