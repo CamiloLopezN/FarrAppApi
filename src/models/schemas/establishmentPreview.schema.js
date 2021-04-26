2c88fa8fab0b4591a871c15d6403eb0d710dc883
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-25 22:36:16 -0500

    :sparkles: attrib location in establishmentPreview.schema.js

--- src/models/schemas/establishmentPreview.schema.js
+++ src/models/schemas/establishmentPreview.schema.js
@@ -1,13 +1,14 @@
 const { Schema } = require('mongoose');
+const locationSchema = require('./location.schema');
 
 const establishmentPreviewSchema = new Schema({
   establishmentId: {
     type: String,
     required: true,
   },
+  companyId: { type: String, required: true },
   establishmentName: { type: String, required: true },
-  city: { type: String, required: true },
-  address: { type: String, required: true },
+  location: { type: locationSchema, required: true },
   imageUrl: { type: String, required: true },
   isActive: { type: Boolean, required: true },
 });
