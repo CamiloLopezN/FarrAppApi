0de6f5f052566101cc6fcb4ec1de0f79ab8002cc
José Díaz Rincón <mail@diazrincon.dev>
2021-04-26 11:33:35 -0500

    :bug: Validation of password not required

--- src/controllers/password.controller.js
+++ src/controllers/password.controller.js
@@ -1,11 +1,9 @@
-const validatorPassCtrl = {};
 const validatorPass = require('../middlewares/validations/password.validator');
 
-validatorPassCtrl.validatePass = async (req, res, next) => {
-  const validation = validatorPass.validate(req.body.password, { list: true });
-  // Dos digitos
-  if (validation.length !== 0) return res.status(400).json({ message: 'bad formated password' });
+module.exports.validatePass = async (req, res, next) => {
+  if (req.body.password != null) {
+    const validation = validatorPass.validate(req.body.password, { list: true });
+    if (validation.length !== 0) return res.status(400).json({ message: 'Bad formated password' });
+  }
   return next();
 };
-
-module.exports = validatorPassCtrl;
