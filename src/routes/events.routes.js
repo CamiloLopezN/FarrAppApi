6287bbc873dfcabedf626d4b1edb9d5d46acb2e4
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-25 20:55:50 -0500

    :recycle: incorrect format path

--- src/routes/events.routes.js
+++ src/routes/events.routes.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const controllerEvents = require('../controllers/events.controller');
 
-router.get('', controllerEvents.getAllEvents);
+router.get('/', controllerEvents.getAllEvents);
 
 module.exports = router;
