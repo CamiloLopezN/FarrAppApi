ff2fbf9fd9df94fd2ab6344aff3e2c04c72fc52b
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-04-29 12:24:51 -0500

    :sparkles: method for routes public of establishment by Id

--- src/middlewares/oauth/authentication.js
+++ src/middlewares/oauth/authentication.js
@@ -24,6 +24,25 @@ oauth.authentication = async (req, res, next) => {
   return true;
 };
 
+oauth.authenticationOrPublic = async (req, res, next) => {
+  if (req.headers.authorization) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: 'Acceso no autorizado, se debe proporcionar un token valido' }); // if there isn't any token
+
+    await jwt.verify(token, config.secretKey, (err, payload) => {
+      if (err) return res.status(403).json({ message: 'Usuario invalido, el token no es valido' });
+      req.payload = payload;
+      if (!payload.role === roles.company) return res.status(403).json({ message: 'Forbidden' });
+      req.id = payload.roleId;
+    });
+  }
+  return next();
+};
+
 oauth.authorizationAdmin = async (req, res, next) => {
   const { payload } = req;
   if (!payload.role === roles.admin) return res.status(403).json({ message: 'Forbidden' });
