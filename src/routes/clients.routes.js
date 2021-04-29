d227b4beee003818f06217d354f09a27cba2c1cb
José Díaz Rincón <mail@diazrincon.dev>
2021-04-28 21:56:14 -0500

    :truck: HTTP routes "/update" removed

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -4,10 +4,10 @@ const router = Router();
 const clientController = require('../controllers/clients.controller');
 
 router.post('/', clientController.postClient);
-router.get('/:clientId', clientController.getClientById);
 router.get('/', clientController.getClients);
-router.post('/:clientId', clientController.updateClientProfile);
 router.post('/follow-establishment', clientController.followEstablishment);
 router.post('/event-interest', clientController.interestForEvent);
+router.get('/:clientId', clientController.getClientById);
+router.post('/:clientId', clientController.updateClientProfile);
 
 module.exports = router;
