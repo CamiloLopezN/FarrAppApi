27cf8294298386bfbd24df3519a155e327e405ee
dijad <finalcooldown@gmail.com>
2021-04-30 14:32:43 -0500

    ♻️ correcion status y mensaje de respuesta

--- src/controllers/establishments.controller.js
+++ src/controllers/establishments.controller.js
@@ -31,7 +31,7 @@ const postReviewEstablishment = async (req, res) => {
       return res.status(400).json({ message: 'Incomplete or bad formatted client data' });
     return res.status(500).json({ message: 'Internal server error' });
   }
-  return res.status(200).json({ message: estReview });
+  return res.status(201).json({ estReview });
 };
 module.exports.postReviewEstablishment = [
   authentication,
--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -53,7 +53,7 @@ const postReviewEvent = async (req, res) => {
       return res.status(400).json({ message: 'Incomplete or bad formatted client data' });
     return res.status(500).json({ message: 'Internal server error' });
   }
-  return res.status(200).json({ message: eventReview });
+  return res.status(201).json({ eventReview });
 };
 
 module.exports.postReviewEvent = [
