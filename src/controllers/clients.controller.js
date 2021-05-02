4294758be04576e9132d3176065cada45a817b8f
jehernandezv <jhonn.hernandez@uptc.edu.co>
2021-05-02 17:37:51 -0500

    :recycle: response err 500

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -66,7 +66,7 @@ const postClient = async (req, res) => {
       return res
         .status(400)
         .json({ message: 'Incomplete or bad formatted client data', errors: error.errors });
-    return res.status(500).json({ message: 'Internal server error', error });
+    return res.status(500).json({ message: 'Internal server error' });
   }
   return res.status(200).json({
     message: 'Successful operation',
